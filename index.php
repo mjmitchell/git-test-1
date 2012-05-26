74b925138cfc399ae5f19b3475d1c9977624e71d
mjmitchell <mike@lepid.com>
2012-05-25 17:20:43 -0700

    Testing - index and stylesheet

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<link href="style.css" media="screen" rel="stylesheet" type="text/css" />
+		<title>Git Test 1</title>
+	</head>
+	<body>
+		<h1>Git Test 1</h1>
+	</body>
+</html>
